edbef0bc55771cb4a72ddddca96328fb0a0a7735
joshkim999 <63277870+joshkim999@users.noreply.github.com>
2020-05-03 14:17:37 -0700

    Update test-get_prev_temp.R

--- tests/testthat/test-get_prev_temp.R
+++ tests/testthat/test-get_prev_temp.R
@@ -1,9 +1,9 @@
 test_that("testing get_prev_weather", {
 
-  correct_result_row <- (142 * 3) + 2
+  correct_result_row <- 288
   correct_result_col <- 6
 
-  my_result <- get_prev_temp(35.2828, -120.6596, 3, "keKglY8JGUawtl9Roz00cFDxokssxVpA")
+  my_result <- get_prev_temp(35.2828, -120.6596, 2, "keKglY8JGUawtl9Roz00cFDxokssxVpA")
 
   expect_equal(nrow(my_result), correct_result_row)
   expect_equal(ncol(my_result), correct_result_col)
