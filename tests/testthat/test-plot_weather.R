a1a00fbe08d682519900db688083c4c7fb675ccf
Delaney Oliver <dcoliver@calpoly.edu>
2020-05-03 22:51:44 -0500

    changing tests

--- tests/testthat/test-plot_weather.R
+++ tests/testthat/test-plot_weather.R
@@ -1,23 +1,18 @@
 test_that("testing radius", {
 
-  correct_result_row <- 1453
-  correct_result_col <- 7
-
   my_result <- radius(35.2828, -120.6596, 3, "wuef6wJsQ3A0wwh9IMFsT8unWLjc5wts")
 
-  expect_equal(nrow(my_result), correct_result_row, tolerance = 10)
-  expect_equal(ncol(my_result), correct_result_col)
+  expect_equal(nrow(my_result), 1453, tolerance = 10)
+  expect_equal(ncol(my_result), 7)
 
   })
 
 
 test_that("testing plot_weather", {
 
-  correct_result <- TRUE
-
   my_result <- plot_weather(35.2828, -120.6596, 3, "F", "wuef6wJsQ3A0wwh9IMFsT8unWLjc5wts")
 
-  expect_equal(is.ggplot(my_result), correct_result)
+  expect_equal(is.ggplot(my_result), TRUE)
 
 
 })
