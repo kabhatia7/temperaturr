ec938706058eff4e029210401b94faa01244b63d
Karan Bhatia <kabhatia7@gmail.com>
2020-05-03 21:49:46 -0700

    test_that

--- tests/testthat/test-plot_weather.R
+++ tests/testthat/test-plot_weather.R
@@ -1,6 +1,6 @@
 test_that("testing radius", {
 
-  my_result <- radius(35.2828, -120.6596, 3, "wuef6wJsQ3A0wwh9IMFsT8unWLjc5wts")
+  my_result <- radius(35.2828, -120.6596, 3, "keKglY8JGUawtl9Roz00cFDxokssxVpA")
 
   expect_equal(nrow(my_result), 1453, tolerance = 10)
   expect_equal(ncol(my_result), 7)
@@ -10,7 +10,7 @@ test_that("testing radius", {
 
 test_that("testing plot_weather", {
 
-  my_result <- plot_weather(35.2828, -120.6596, 3, "F", "wuef6wJsQ3A0wwh9IMFsT8unWLjc5wts")
+  my_result <- plot_weather(35.2828, -120.6596, 3, "F", "keKglY8JGUawtl9Roz00cFDxokssxVpA")
 
   expect_equal(is.ggplot(my_result), TRUE)
 
