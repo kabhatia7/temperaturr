dcfee12e596d2598953982e002412dd7209c8a1d
mbostrom11 <63316027+mbostrom11@users.noreply.github.com>
2020-05-03 20:21:38 -0700

    Add files via upload

---
+++ tests/test_Weather_Comparison.R
@@ -0,0 +1,10 @@
+test_that("testing Weather_Comparison", {
+
+  correct_result_row <- 24
+  correct_result_col <- 8
+
+  my_result <- Weather_Comparison(35.2828, -120.6596, "keKglY8JGUawtl9Roz00cFDxokssxVpA")
+
+  expect_equal(nrow(my_result), correct_result_row)
+  expect_equal(ncol(my_result), correct_result_col)
+})
